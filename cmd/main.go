754c6104a1863a69530a3542e756506fb63b41d9
ryuji0123 <rokuda1011@gmail.com>
2024-06-29 20:56:29 +0900

    Add POST and DELETE api skeletons for posts.

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,26 @@ func main() {
 	}
 
 	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World")
+		fmt.Fprintf(w, "Hello, World\n")
+	})
+
+	http.HandleFunc("/api/posts", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			fmt.Fprintf(w, "Received Post request for posts.\n")
+		case http.MethodDelete:
+			fmt.Fprintf(w, "Received Delete request for posts.\n")
+		default:
+			http.Error(w, fmt.Sprintln("/api/posts supports only Post and Delete now."), http.StatusHTTPVersionNotSupported)
+		}
+	})
+
+	http.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Users\n")
+	})
+
+	http.HandleFunc("/api/notifications", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Notifications\n")
 	})
 
 	log.Println("Starting server...")
