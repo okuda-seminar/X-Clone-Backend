f7395f34e732080981f8fab4f3eddf87c212ddba
ryuju0911 <pinebook0911@gmail.com>
2024-07-23 20:06:19 +0900

    [Users] Implement TestCreateUser.

--- api/handlers/handlers_test.go
+++ api/handlers/handlers_test.go
@@ -1,16 +1,47 @@
 package handlers
 
 import (
-	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 )
 
-// TODO: https://github.com/okuda-seminar/X-Clone-Backend/issues/63
-// - [Users] Implement TestCreateUser
 func (s *HandlersTestSuite) TestCreateUser() {
-	log.Println("TestCreateUser was called.")
+	tests := []struct {
+		name         string
+		body         string
+		expectedCode int
+	}{
+		{
+			name:         "create user",
+			body:         `{ "username": "test", "display_name": "test" }`,
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid body",
+			body:         `{ "invalid": "test" }`,
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "duplicated username",
+			body:         `{ "username": "test", "display_name": "duplicated" }`,
+			expectedCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest("POST", "/api/users", strings.NewReader(test.body))
+		rr := httptest.NewRecorder()
+
+		CreateUser(rr, req, s.db)
+
+		if rr.Code != test.expectedCode {
+			s.T().Errorf("%s: wrong code returned; expected %d, but got %d", test.name, test.expectedCode, rr.Code)
+		}
+	}
 }
 
 // TestHandlersTestSuite runs all of the tests attached to HandlersTestSuite.
