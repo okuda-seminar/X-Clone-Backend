ec0d5a6a894c5269d3291a96ef72a16384aacd9c
nayuta-ai <nayuta723@gmail.com>
2024-07-27 23:37:07 +0900

    [Users] Implement TestCreateMuting.

--- api/handlers/handlers_test.go
+++ api/handlers/handlers_test.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"x-clone-backend/entities"
 
 	"github.com/stretchr/testify/suite"
 )
@@ -42,6 +44,77 @@ func (s *HandlersTestSuite) TestCreateUser() {
 			s.T().Errorf("%s: wrong code returned; expected %d, but got %d", test.name, test.expectedCode, rr.Code)
 		}
 	}
+}
+
+func (s *HandlersTestSuite) TestCreateMuting() {
+	// CreateMuting must use existing user IDs from the user table
+	// for both the source user ID and target user ID.
+	// Therefore, users are created for testing purposes to obtain these IDs.
+	sourceUserId := s.getTestUserId(`{ "username": "test", "display_name": "test" }`)
+	targetUserId := s.getTestUserId(`{ "username": "test2", "display_name": "test2" }`)
+
+	tests := []struct {
+		name         string
+		body         string
+		expectedCode int
+	}{
+		{
+			name:         "create muting",
+			body:         `{ "target_user_id": "` + targetUserId + `" }`,
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid JSON body",
+			body:         `{ "target_user_id": "` + targetUserId,
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "invalid body",
+			body:         `{ "invalid": "test" }`,
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "duplicated muting",
+			body:         `{ "target_user_id": "` + targetUserId + `" }`,
+			expectedCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest(
+			"POST",
+			"/api/users/{id}/muting",
+			strings.NewReader(test.body),
+		)
+		req.SetPathValue("id", sourceUserId)
+
+		rr := httptest.NewRecorder()
+		CreateMuting(rr, req, s.db)
+
+		if rr.Code != test.expectedCode {
+			s.T().Errorf(
+				"%s: wrong code returned; expected %d, but got %d",
+				test.name,
+				test.expectedCode,
+				rr.Code,
+			)
+		}
+	}
+}
+
+func (s *HandlersTestSuite) getTestUserId(body string) string {
+	req := httptest.NewRequest(
+		"POST",
+		"/api/users",
+		strings.NewReader(body),
+	)
+	rr := httptest.NewRecorder()
+	CreateUser(rr, req, s.db)
+
+	var user entities.User
+	_ = json.NewDecoder(rr.Body).Decode(&user)
+	sourceUserId := user.ID.String()
+	return sourceUserId
 }
 
 // TestHandlersTestSuite runs all of the tests attached to HandlersTestSuite.
