27e063c4d4159662996efc00a24f62ebe47fb8b6
summerive <92002444+summerive@users.noreply.github.com>
2024-08-04 01:29:42 +0900

    [Posts] Implement TestLikePost.

--- api/handlers/handlers_test.go
+++ api/handlers/handlers_test.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 	"x-clone-backend/entities"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -47,6 +49,87 @@ func (s *HandlersTestSuite) TestCreateUser() {
 
 		if rr.Code != test.expectedCode {
 			s.T().Errorf("%s: wrong code returned; expected %d, but got %d", test.name, test.expectedCode, rr.Code)
+		}
+	}
+}
+
+func (s *HandlersTestSuite) TestLikePost() {
+	// LikePost must use existing user ID and post ID
+	// from the users and posts table.
+	// Therefore, users and posts are created
+	// for testing purposes to obtain these IDs.
+	authorUserId := s.getTestUserId(`{ "username": "author", "display_name": "author" }`)
+	likerUserId := s.getTestUserId(`{ "username": "liker", "display_name": "liker" }`)
+	postId := s.getTestPostId(fmt.Sprintf(`{ "user_id": "%s", "text": "test post"}`, authorUserId))
+
+	tests := []struct {
+		name         string
+		userId       string
+		body         string
+		expectedCode int
+	}{
+		{
+			name:         "create like for a own post",
+			userId:       authorUserId,
+			body:         fmt.Sprintf(`{ "post_id": "%s" }`, postId),
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "create like for other's post",
+			userId:       likerUserId,
+			body:         fmt.Sprintf(`{ "post_id": "%s" }`, postId),
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid JSON body",
+			userId:       likerUserId,
+			body:         fmt.Sprintf(`{ "post_id": "%s"`, postId),
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "invalid body",
+			userId:       likerUserId,
+			body:         `{ "invalid": "test" }`,
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "non-existent user id",
+			userId:       uuid.New().String(),
+			body:         fmt.Sprintf(`{ "post_id": "%s" }`, postId),
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "non-existent post id",
+			userId:       likerUserId,
+			body:         fmt.Sprintf(`{ "post_id": "%s" }`, uuid.New().String()),
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "duplicated like",
+			userId:       likerUserId,
+			body:         fmt.Sprintf(`{ "post_id": "%s" }`, postId),
+			expectedCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest(
+			"POST",
+			"/api/users/{id}/likes",
+			strings.NewReader(test.body),
+		)
+		req.SetPathValue("id", test.userId)
+
+		rr := httptest.NewRecorder()
+		LikePost(rr, req, s.db)
+
+		if rr.Code != test.expectedCode {
+			s.T().Errorf(
+				"%s: wrong code returned; expected %d, but got %d",
+				test.name,
+				test.expectedCode,
+				rr.Code,
+			)
 		}
 	}
 }
@@ -120,6 +203,21 @@ func (s *HandlersTestSuite) getTestUserId(body string) string {
 	_ = json.NewDecoder(rr.Body).Decode(&user)
 	sourceUserId := user.ID.String()
 	return sourceUserId
+}
+
+func (s *HandlersTestSuite) getTestPostId(body string) string {
+	req := httptest.NewRequest(
+		"POST",
+		"/api/posts",
+		strings.NewReader(body),
+	)
+	rr := httptest.NewRecorder()
+	CreatePost(rr, req, s.db)
+
+	var post entities.Post
+	_ = json.NewDecoder(rr.Body).Decode(&post)
+	postId := post.ID.String()
+	return postId
 }
 
 // TestHandlersTestSuite runs all of the tests attached to HandlersTestSuite.
