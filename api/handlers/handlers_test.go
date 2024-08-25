67a70ddc02dccb8fac7751b8b28e767964c75764
summerive <92002444+summerive@users.noreply.github.com>
2024-08-25 21:39:26 +0900

    [Posts] Implement TestDeletePost.

--- api/handlers/handlers_test.go
+++ api/handlers/handlers_test.go
@@ -53,6 +53,52 @@ func (s *HandlersTestSuite) TestCreateUser() {
 	}
 }
 
+func (s *HandlersTestSuite) TestDeletePost() {
+	userID := s.newTestUser(`{ "username": "test user", "display_name": "test user" }`)
+	postID := s.newTestPost(fmt.Sprintf(`{ "user_id": "%s", "text": "test post"}`, userID))
+
+	tests := []struct {
+		name         string
+		postID       string
+		expectedCode int
+	}{
+		{
+			name:         "delete a post successfully with a proper post ID.",
+			postID:       postID,
+			expectedCode: http.StatusNoContent,
+		},
+		{
+			name:         "fail to delete a post that was already deleted .",
+			postID:       postID,
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "fail to delete a post with a non-existent post ID.",
+			postID:       uuid.New().String(),
+			expectedCode: http.StatusNotFound,
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest("DELETE", "/api/posts{postID}",
+			nil)
+		req.SetPathValue("postID", test.postID)
+
+		rr := httptest.NewRecorder()
+		DeletePost(rr, req, s.db)
+
+		if rr.Code != test.expectedCode {
+			s.T().Errorf(
+				"%s: wrong code returned; expected %d, but got %d",
+				test.name,
+				test.expectedCode,
+				rr.Code,
+			)
+		}
+
+	}
+}
+
 func (s *HandlersTestSuite) TestLikePost() {
 	// LikePost must use existing user ID and post ID
 	// from the users and posts table.
